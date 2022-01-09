455527bca444ac0d2b16df3b69fd605cb6b2ac23
LAPTOP-PATF7J21\Tibame_T14 <a1226667@yahoo.com.tw>
2022-01-09 11:10:13 +0800

    change

--- src/main/java/Miller/Millertest.java
+++ src/main/java/Miller/Millertest.java
@@ -1,10 +1,14 @@
 package Miller;
 
 public class Millertest {
-	System.out.println("Hello");
-	System.out.println("miller");
-	System.out.println("Patrick");
-	System.out.println("Hoga");
-	System.out.println("Hello");
-	System.out.println("Ommmmmmmmm");
+	public static void main(String[] args) {
+
+		System.out.println("Hello");
+		System.out.println("miller");
+		System.out.println("Patrick");
+		System.out.println("Hoga");
+		System.out.println("Hello");
+		System.out.println("Ommmmmmmmm");
+	}
+
 }
