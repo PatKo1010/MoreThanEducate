34eae6a476c381a154e48b119cbf1b31e7d10b4c
LAPTOP-6IR4E4AF\Tibame_T14 <avenger79513@gmail.com>
2022-01-09 11:04:33 +0800

    2222

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,7 +1,7 @@
 import java.time.zone.*;
 public class test {
 		public static void main (String[]args) {
-			System.out.println("yoyoyoyo");
-			System.out.println("2ndyoyoyo");
+			System.out.println("~_~");
+		
 		}
 	}
