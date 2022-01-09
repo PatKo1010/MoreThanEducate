1229ec7daa30fc8fc18e24be6a0391d4a8740c7f
Hoga <howard.huang18@gmail.com>
2022-01-09 10:07:37 +0800

    delete trash, add my name

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,11 +4,9 @@ public class Hello {
 	public static void main(String[] args) {
 
 		System.out.println("Hello");
-<<<<<<< HEAD
 		System.out.println("³¯­³¹F");
-=======
 		System.out.println("Patrick");
->>>>>>> f4628455bc1671c213a3adac35abd2bb48da9413
+		System.out.println("Hoga");
 	}
 
 }
